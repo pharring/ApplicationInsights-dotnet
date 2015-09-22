28c6214ebf0a25a57c283306269d8ac6c11ad533
Mike Smacinih <mihailsm@microsoft.com>
2015-09-22 10:15:48 -0700

    Manual merge from https://mseng.visualstudio.com/DefaultCollection/AppInsights/AI%20Devices%20-%20Code%20Ninjas/_git/Devices-Symbolication-Gem

---
+++ src/Core/Managed/Shared/Extensibility/Implementation/External/CrashData.cs
@@ -0,0 +1,12 @@
+﻿#if UTC
+namespace Microsoft.ApplicationInsights.Extensibility.Implementation.External
+{
+    /// <summary>
+    /// Partial class to add the CrashData and any additional customizations to the generated type.
+    /// </summary>
+    [System.Diagnostics.Tracing.EventData(Name = "PartB_AjaxCallData")]
+    internal partial class CrashData
+    {
+    }
+}
+#endif
---
+++ src/Core/Managed/Shared/Extensibility/Implementation/External/CrashDataBinary.cs
@@ -0,0 +1,11 @@
+﻿#if UTC
+namespace Microsoft.ApplicationInsights.Extensibility.Implementation.External
+{
+    /// <summary>
+    /// Partial class to add the CrashDataBinary and any additional customizations to the generated type.
+    /// </summary>
+    internal partial class CrashDataBinary
+    {
+    }
+}
+#endif
